2b40d40aa0e8ff3ae90f164842f11c08386b9b73
pedroseis27-a11y <pedroseis27@gmail.com>
2026-08-18 15:47:35 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,19 +1,14 @@
-const Stripe = require("stripe");
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 module.exports = async (req, res) => {
   try {
-    const account = await stripe.accounts.retrieve();
+    const response = await fetch("https://api.stripe.com/v1");
 
     res.status(200).json({
       ok: true,
-      message: "Stripe conectado correctamente 🎉",
-      country: account.country
+      status: response.status
     });
 
   } catch (error) {
-    console.error("STRIPE ERROR:", error);
+    console.error(error);
 
     res.status(500).json({
       ok: false,
